8b2b1c95dbbabcedf2eef8c7ae6d9bf2b994c0aa
Batuhan Ham <batu@Batuhan-MacBook-Air.local>
2024-08-12 23:47:55 +0300

    proje tamamlandı

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,22 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Batuhan Ham on 7.08.2024.
+//
+import UIKit
+import Foundation
+
+class Simpson {
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName:String, simpsonJob:String, simpsonImage:UIImage){
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+    }
+    
+}
